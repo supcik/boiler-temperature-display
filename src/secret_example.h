2bd1aca9bfde63125b521ebed35ebab93eb3631f
Jacques Supcik <jacques@supcik.net>
2021-11-10 23:08:39 +0100

    add example of "secret.h"

---
+++ src/secret_example.h
@@ -0,0 +1,13 @@
+#ifndef SECRET_H_
+#define SECRET_H_
+
+#include <Arduino.h>
+
+const String kUrl = "http://...";
+const String kTitle = "TITLE";
+
+const String kSSID = "MyWifiSSID";
+const String kPassPhrase= "MyWifiPassphrase";
+
+
+#endif /* SECRET_H_ */
